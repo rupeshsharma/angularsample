0c339e5e8f0b2892d4549b7a444148b024db4232
Rupesh Sharma <rupesh.jksharma@gmail.com>
2017-07-30 14:19:13 +0530

    menu template changes

--- component/menu/menu.component.js
+++ component/menu/menu.component.js
@@ -2,13 +2,18 @@ angular.
   module('sample').
   component('menu', {
     templateUrl: './component/menu/menu.template.html',
-    controller: ['$scope', 'sessionService',
-      function menuController($scope, sessionService) {
+    controller: ['$scope', '$location','sessionService',
+      function menuController($scope, $location, sessionService) {
         $scope.pageName = "Menu ******";
 var userData = sessionService.getUserData();
 $scope.mobile = userData.mobile;
 $scope.name = userData.name;
         console.log("********"+JSON.stringify(userData));
+
+        $scope.clearSession = function(){
+          sessionService.clearUserSession();
+          $location.path('/');
+        }
       }
     ]
   });
