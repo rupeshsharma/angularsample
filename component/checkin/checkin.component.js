7d9bec8ce9b65bb5638f2e6293a68a5bf965f64a
rupeshsharma <rupesh.sharma@globallogic.com>
2017-08-03 13:56:30 +0530

    Update checkin.component.js

--- component/checkin/checkin.component.js
+++ component/checkin/checkin.component.js
@@ -5,6 +5,21 @@ angular.
     controller: ['$scope','$location','sessionService','userService',
       function checkInController($scope, $location, sessionService, userService) {
 
+$('#login-form-link').click(function(e) {
+		$("#login-form").delay(100).fadeIn(100);
+ 		$("#register-form").fadeOut(100);
+		$('#register-form-link').removeClass('active');
+		$(this).addClass('active');
+		e.preventDefault();
+	});
+	$('#register-form-link').click(function(e) {
+		$("#register-form").delay(100).fadeIn(100);
+ 		$("#login-form").fadeOut(100);
+		$('#login-form-link').removeClass('active');
+		$(this).addClass('active');
+		e.preventDefault();
+	});
+
         $scope.checkIn = function(isAnonymous){
           if(!isAnonymous){
             sessionService.isAnonymousCustomer(false);
