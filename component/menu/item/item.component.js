efb40869fd54136b9c6284d5933741d699e809e3
rupeshsharma <rupesh.sharma@globallogic.com>
2017-08-03 13:59:47 +0530

    Update item.component.js

--- component/menu/item/item.component.js
+++ component/menu/item/item.component.js
@@ -2,10 +2,39 @@ angular.
   module('sample').
   component('item', {
     templateUrl: './component/menu/item/item.template.html',
-    controller: ['$scope','$location','sessionService','userService',
-      function itemController($scope, $location, sessionService, userService) {
+    controller: ['$scope','$location','sessionService','menuService',
+      function itemController($scope, $location, sessionService, menuService) {
+        setUpMenu(menuService);
+        $scope.itemList = [];
 
+        function setUpMenu(menuService){
+          menuService.getMenu(resp => {
+            $scope.menu = resp;
+          })
+        }
 
+        $scope.displayItemsForCategory = function(categorySelected){
+          if($scope.categorySelected && $scope.categorySelected!=''){
+          $scope.itemList = $scope.menu[$scope.menu.indexOf($scope.categorySelected)].items;
+          }else{
+            $scope.itemList = [];
+          }
+        }
+
+        $scope.addItem =function(){
+          if($scope.newItemName != '' && $scope.newItemPrice != ''){
+            var newItem = {
+            "itemName" : $scope.newItemName,
+            "itemPrice": $scope.newItemPrice
+            }
+            $scope.menu[$scope.menu.indexOf($scope.categorySelected)].items.push(newItem);
+            $scope.itemList = $scope.menu[$scope.menu.indexOf($scope.categorySelected)].items;
+            $scope.newItemName = '';
+            $scope.newItemPrice = '';
+          }
+           $("#myModal .close").click()
+        }
+       
       }
     ]
   });
