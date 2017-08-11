3e5d157c8ad98cfe909666e0834fba0aa73e5e53
rupeshsharma <rupesh.sharma@globallogic.com>
2017-08-11 18:45:24 +0530

    Update customer.component.js

--- component/customer/customer.component.js
+++ component/customer/customer.component.js
@@ -2,10 +2,18 @@ angular.
   module('sample').
   component('customer', {
     templateUrl: './component/customer/customer.template.html',
-    controller: ['$scope','$location','sessionService','userService',
+    controller: ['$scope', '$location', 'sessionService', 'userService',
       function customerController($scope, $location, sessionService, userService) {
-
-
+        $("#fromDate").datepicker({
+          changeMonth: true,
+          changeYear: true,
+          dateFormat: "dd-mm-yy"
+        });
+        $("#toDate").datepicker({
+          changeMonth: true,
+          changeYear: true,
+          dateFormat: "dd-mm-yy"
+        });
       }
     ]
   });
