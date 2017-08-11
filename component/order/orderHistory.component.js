13e68550db42cda2695e99090723147d3e7af2d5
Rupesh Sharma <rupesh.jksharma@gmail.com>
2017-08-12 00:57:01 +0530

    update

--- component/order/orderHistory.component.js
+++ component/order/orderHistory.component.js
@@ -5,6 +5,16 @@ angular.
     controller: ['$scope','$location','sessionService','userService',
       function customerController($scope, $location, sessionService, userService) {
 
+        $("#fromViewOrderDate").datepicker({
+          changeMonth: true,
+          changeYear: true,
+          dateFormat: "dd-mm-yy"
+        });
+        $("#toViewOrderDate").datepicker({
+          changeMonth: true,
+          changeYear: true,
+          dateFormat: "dd-mm-yy"
+        });
 
       }
     ]
