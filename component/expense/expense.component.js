7b65bd9c398404c7b7c18f43e9cd7546bd95223b
rupeshsharma <rupesh.sharma@globallogic.com>
2017-08-11 18:55:34 +0530

    Update expense.component.js

--- component/expense/expense.component.js
+++ component/expense/expense.component.js
@@ -4,6 +4,18 @@ angular.
     templateUrl: './component/expense/expense.template.html',
     controller: ['$scope','$filter', '$location', 'sessionService', 'expenseService',
       function expenseController($scope, $filter, $location, sessionService, expenseService) {
+
+        $("#fromExpenseDate").datepicker({
+          changeMonth: true,
+          changeYear: true,
+          dateFormat: "dd-mm-yy"
+        });
+        $("#toExpenseDate").datepicker({
+          changeMonth: true,
+          changeYear: true,
+          dateFormat: "dd-mm-yy"
+        });
+
         $scope.dateOfExpense = $filter('date')(new Date(), "dd-MM-yyyy");
         $("#dateOfExpense").datepicker({
           changeMonth: true,
