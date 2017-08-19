ef3a4a9eb5bf078d37dfd0d6c0d110f90c7efb4a
Rupesh Sharma <rupesh.jksharma@gmail.com>
2017-08-20 00:56:33 +0530

    update

--- component/checkin/checkin.component.js
+++ component/checkin/checkin.component.js
@@ -2,24 +2,9 @@ angular.
   module('sample').
   component('checkIn', {
     templateUrl: './component/checkin/checkin.template.html',
-    controller: ['$scope','$location','sessionService','userService',
-      function checkInController($scope, $location, sessionService, userService) {
-
-$('#login-form-link').click(function(e) {
-		$("#login-form").delay(100).fadeIn(100);
- 		$("#register-form").fadeOut(100);
-		$('#register-form-link').removeClass('active');
-		$(this).addClass('active');
-		e.preventDefault();
-	});
-	$('#register-form-link').click(function(e) {
-		$("#register-form").delay(100).fadeIn(100);
- 		$("#login-form").fadeOut(100);
-		$('#login-form-link').removeClass('active');
-		$(this).addClass('active');
-		e.preventDefault();
-	});
-
+    controller: ['$scope', '$rootScope', '$location','sessionService','userService',
+      function checkInController($scope, $rootScope, $location, sessionService, userService) {
+        $rootScope.viewType = 'checkin';
         $scope.checkIn = function(isAnonymous){
           if(!isAnonymous){
             sessionService.isAnonymousCustomer(false);
