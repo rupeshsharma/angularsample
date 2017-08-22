0f619c0f3ed6c75655cd881dfee1fa45e2cb4d35
rupeshsharma <rupesh.sharma@globallogic.com>
2017-08-22 15:04:09 +0530

    Update staff.component.js

--- component/staff/staff.component.js
+++ component/staff/staff.component.js
@@ -2,19 +2,28 @@ angular.
   module('sample').
   component('staff', {
     templateUrl: './component/staff/staff.template.html',
-    controller: ['$scope', '$location', 'sessionService', 'userService',
-      function staffController($scope, $location, sessionService, userService) {
+    controller: ['$scope', '$rootScope', '$location', 'sessionService', 'userService',
+      function staffController($scope, $rootScope, $location, sessionService, userService) {
+
+        $rootScope.$on('rootScope:staffInit', function (event, data) {
+           $scope.staffInit();
+        });
+
+        $scope.staffInit = function () {
+          console.debug("Staff INIT");
+        }
+
         $scope.staffList = [
           {
-"id":1,
-"name":"Rupesh Sharma",
-"mobile":"8826479790",
-"type":"Admin"
-          },{
-"id":2,
-"name":"Devendra Sharma",
-"mobile":"9691639313",
-"type":"Admin"
+            "id": 1,
+            "name": "Rupesh Sharma",
+            "mobile": "8826479790",
+            "type": "Admin"
+          }, {
+            "id": 2,
+            "name": "Devendra Sharma",
+            "mobile": "9691639313",
+            "type": "Admin"
           }
 
         ];
