3dd43361d3ae26e1f03f2e05c10377fdb5b431c2
rupeshsharma <rupesh.sharma@globallogic.com>
2017-08-22 15:02:38 +0530

    Update customer.component.js

--- component/customer/customer.component.js
+++ component/customer/customer.component.js
@@ -2,8 +2,17 @@ angular.
   module('sample').
   component('customer', {
     templateUrl: './component/customer/customer.template.html',
-    controller: ['$scope', '$location', 'sessionService', 'userService',
-      function customerController($scope, $location, sessionService, userService) {
+    controller: ['$scope', '$rootScope', '$location', 'sessionService', 'userService',
+      function customerController($scope, $rootScope, $location, sessionService, userService) {
+
+        $rootScope.$on('rootScope:customerInit', function (event, data) {
+           $scope.customerInit();
+        });
+
+         $scope.customerInit = function () {
+          console.debug("Customer INIT");
+        }
+
         $("#fromDate").datepicker({
           changeMonth: true,
           changeYear: true,
