87379b15e511a1068e1939807343169c5a85e571
rupeshsharma <rupesh.sharma@globallogic.com>
2017-09-12 15:11:22 +0530

    Update expenseService.js

--- service/expenseService.js
+++ service/expenseService.js
@@ -1,16 +1,30 @@
 var APP = angular.module('sample')
 
 APP.factory('expenseService', ['httpService',
-    function(httpService) {
+    function (httpService) {
 
         return {
-            getAllExpenses: function getAllExpenses (callback){
-            httpService.getAllExpenses().then(
-            resp => {
-                callback(resp.data); 
+            getTodaysExpenses: function getTodaysExpenses(callback) {
+                httpService.getTodaysExpenses().then(
+                    resp => {
+                        callback(resp.data);
+                    }
+                );
+            },
+            addOrUpdateExpense: function addOrUpdateExpense(request, callback) {
+                httpService.addOrUpdateExpense(request).then(
+                    resp => {
+                        callback(resp.data);
+                    }
+                );
+            },
+            searchExpensesInRange: function searchExpensesInRange(fromDate, toDate, callback){
+                httpService.searchExpensesInRange(fromDate, toDate).then(
+                    resp => {
+                        callback(resp.data);
+                    }
+                );
             }
-        );
-       }
-      }        
+        }
     }
-  ]);
+]);
