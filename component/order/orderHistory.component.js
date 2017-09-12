e4c7bd28bdbdc13149c134d2455de79c014b84cf
rupeshsharma <rupesh.sharma@globallogic.com>
2017-09-12 17:47:39 +0530

    Update orderHistory.component.js

--- component/order/orderHistory.component.js
+++ component/order/orderHistory.component.js
@@ -2,32 +2,75 @@ angular.
   module('sample').
   component('orderHistory', {
     templateUrl: './component/order/orderHistory.template.html',
-    controller: ['$scope', '$timeout', '$rootScope', '$location','sessionService','userService',
-      function customerController($scope, $timeout, $rootScope, $location, sessionService, userService) {
+    controller: ['$scope', '$timeout', '$rootScope', '$location', 'sessionService', 'orderHistoryService',
+      function customerController($scope, $timeout, $rootScope, $location, sessionService, orderHistoryService) {
 
         $scope.$on('orderInit', function (event) {
-           $scope.orderInit();
+          $scope.orderInit();
         });
 
-         $scope.orderInit = function () {
-           document.getElementById("loadingIndicator").style.display = 'block';
-           document.getElementById("orderComponent").style.display = 'none';
-           $timeout(function () {
-             document.getElementById("loadingIndicator").style.display = 'none';
-             document.getElementById("orderComponent").style.display = 'block';
-        }, 500);
-          console.debug("Order INIT");
+        $scope.orderInit = function () {
+          $scope.getTodayOrderList();
+        }
+
+        function closeLoadingIndicator() {
+          document.getElementById("loadingIndicator").style.display = 'none';
+          document.getElementById("orderComponent").style.display = 'block';
+        }
+
+        $scope.getTodayOrderList = function () {
+          document.getElementById("loadingIndicator").style.display = 'block';
+          document.getElementById("orderComponent").style.display = 'none';
+          orderHistoryService.getTodayOrderList(data => {
+            $scope.orderHistoryList = data;
+            closeLoadingIndicator();
+          });
+
+        }
+
+        $scope.getOrderDetailById = function (orderId) {
+          $scope.showOrderDetailView = false;
+          document.getElementById("orderDetailLoadingIndicator").style.display = 'block';
+          orderHistoryService.getOrderDetailById(orderId, data => {
+            $scope.orderDetail = data;
+            document.getElementById("orderDetailLoadingIndicator").style.display = 'none';
+            $scope.showOrderDetailView = true;
+          });
+        }
+
+        $scope.searchOrderHistoryInRange = function () {
+          if ($scope.fromViewOrderHistoryDate != undefined && $scope.fromViewOrderHistoryDate != '' && $scope.toViewOrderHistoryDate != undefined && $scope.toViewOrderHistoryDate != '') {
+
+            if ($scope.fromViewOrderHistoryDate > $scope.toViewOrderHistoryDate) {
+              alert('From Date cannot be greater than To Date');
+            } else {
+              document.getElementById("loadingIndicator").style.display = 'block';
+              document.getElementById("orderComponent").style.display = 'none';
+              console.log($scope.fromViewOrderHistoryDate + " " + $scope.toViewOrderHistoryDate);
+              orderHistoryService.searchOrderHistoryInRange($scope.fromViewOrderHistoryDate, $scope.toViewOrderHistoryDate, data => {
+                $scope.orderHistoryList = data;
+                closeLoadingIndicator();
+              });
+            }
+
+          }
         }
 
         $("#fromViewOrderDate").datepicker({
           changeMonth: true,
           changeYear: true,
-          dateFormat: "dd-mm-yy"
+          dateFormat: "dd-mm-yy",
+          onSelect: function (date) {
+            $scope.fromViewOrderHistoryDate = date;
+          }
         });
         $("#toViewOrderDate").datepicker({
           changeMonth: true,
           changeYear: true,
-          dateFormat: "dd-mm-yy"
+          dateFormat: "dd-mm-yy",
+          onSelect: function (date) {
+            $scope.toViewOrderHistoryDate = date;
+          }
         });
 
       }
