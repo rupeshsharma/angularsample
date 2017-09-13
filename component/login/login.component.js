8c67120e7a805289cb34afa822a25b6011691590
rupeshsharma <rupesh.sharma@globallogic.com>
2017-09-13 11:51:54 +0530

    Update login.component.js

--- component/login/login.component.js
+++ component/login/login.component.js
@@ -5,16 +5,6 @@ angular.
     controller: ['$scope', '$timeout', '$rootScope', '$location', 'sessionService', 'customerService', 'masterService',
       function loginController($scope, $timeout, $rootScope, $location, sessionService, customerService, masterService) {
 
-        $rootScope.logOut = function () {
-          for (var prop in $rootScope) {
-            if (prop.substring(0, 1) !== '$') {
-              delete $rootScope[prop];
-            }
-          }
-          sessionService.clearUserSession();
-          $location.path('/');
-        }
-
         $scope.doLogin = function () {
           $("#loginComponent").addClass("disabledDiv");
           document.getElementById("loadingIndicator").style.display = 'block';
