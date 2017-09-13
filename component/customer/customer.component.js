741b8b60ac72455b7bdffd88d90790f484b0c620
rupeshsharma <rupesh.sharma@globallogic.com>
2017-09-13 18:19:38 +0530

    Update customer.component.js

--- component/customer/customer.component.js
+++ component/customer/customer.component.js
@@ -2,33 +2,58 @@ angular.
   module('sample').
   component('customer', {
     templateUrl: './component/customer/customer.template.html',
-    controller: ['$scope', '$timeout', '$rootScope', '$location', 'sessionService', 'customerService',
-      function customerController($scope, $timeout, $rootScope, $location, sessionService, customerService) {
+    controller: ['$scope', '$timeout', '$rootScope', '$location', 'sessionService', 'customerService', 'orderHistoryService',
+      function customerController($scope, $timeout, $rootScope, $location, sessionService, customerService, orderHistoryService) {
 
         $scope.$on('customerInit', function (event) {
-           $scope.customerInit();
+          $scope.customerInit();
         });
 
-         $scope.customerInit = function () {
-           document.getElementById("loadingIndicator").style.display = 'block';
-           document.getElementById("customerComponent").style.display = 'none';
-           $timeout(function () {
-             document.getElementById("loadingIndicator").style.display = 'none';
-             document.getElementById("customerComponent").style.display = 'block';
-        }, 500);
-          console.debug("Customer INIT");
+        $scope.customerInit = function () {
+          closeLoadingIndicator();
+          $scope.customerList = [];
         }
 
-        $("#fromDate").datepicker({
+        $scope.searchLastVisited = function () {
+          if ($scope.lastVisitedDate != undefined && $scope.lastVisitedDate != '') {
+            document.getElementById("loadingIndicator").style.display = 'block';
+            document.getElementById("customerComponent").style.display = 'none';
+            customerService.searchCustomerVisitedBefore($scope.lastVisitedDate, data => {
+              $scope.customerList = data;
+              closeLoadingIndicator();
+            })
+          }
+        }
+
+        $scope.showCustomerHistory = function (customer) {
+          $scope.selectedCustomer = customer;
+          customerService.getCustomerHistory(customer.id, data => {
+            $scope.customerOrderHistoryList = data;
+          });
+        }
+
+        $scope.viewOrderDetail = function (id){
+          orderHistoryService.getOrderDetailById(id, data => {
+            $scope.orderDetail = data;
+          });
+        }
+
+        function closeLoadingIndicator() {
+          document.getElementById("loadingIndicator").style.display = 'none';
+          document.getElementById("customerComponent").style.display = 'block';
+        }
+
+        $("#lastVisitedDate").datepicker({
           changeMonth: true,
           changeYear: true,
-          dateFormat: "dd-mm-yy"
+          dateFormat: "dd-mm-yy",
+          maxDate: -1,
+          onSelect: function (date) {
+            $scope.lastVisitedDate = date;
+          }
+
         });
-        $("#toDate").datepicker({
-          changeMonth: true,
-          changeYear: true,
-          dateFormat: "dd-mm-yy"
-        });
+
       }
     ]
   });
