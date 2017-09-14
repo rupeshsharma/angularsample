96dcd03284a524a4db4d6b722fb98b4ad302cd9f
rupeshsharma <rupesh.sharma@globallogic.com>
2017-09-14 14:35:30 +0530

    Update dashboard.component.js

--- component/dashboard/dashboard.component.js
+++ component/dashboard/dashboard.component.js
@@ -30,105 +30,11 @@ angular.
           delete $rootScope.viewButtonClicked;
         }
 
-        $scope.chart = new CanvasJS.Chart("ct-visits", {
-          title: {
-            text: "Adding & Updating dataPoints"
-          },
-          data: [
-            {
-              // Change type to "doughnut", "line", "splineArea", etc.
-              type: "column",
-              dataPoints: [
-                { label: "1/7/2017", y: 10 },
-                { label: "8/7/2017", y: 15 },
-                { label: "15/7/2017", y: 25 },
-                { label: "21/7/2017", y: 30 },
-                { label: "28/8/2017", y: 28 }
-              ]
-            },
-            {
-              // Change type to "doughnut", "line", "splineArea", etc.
-              type: "line",
-              dataPoints: [
-                { label: "1/7/2017", y: 11 },
-                { label: "8/7/2017", y: 14 },
-                { label: "15/7/2017", y: 29 },
-                { label: "21/7/2017", y: 40 },
-                { label: "28/8/2017", y: 58 }
-              ]
-            }
-          ]
-        });
-        $scope.updateChart = function (updateBy) {
-          $scope.renderChartBy = updateBy;
-          if (updateBy == 'm') {
-            $scope.chart.options.title.text = "Monthly";
-            $scope.chart.options.data[0].dataPoints = [
-              { label: "Jan", y: 10 },
-              { label: "Feb", y: 15 },
-              { label: "Mar", y: 25 },
-              { label: "Apr", y: 30 },
-              { label: "May", y: 28 },
-              { label: "Jun", y: 28 },
-              { label: "Jul", y: 50 },
-              { label: "Aug", y: 28 },
-              { label: "Sep", y: 60 },
-              { label: "Oct", y: 28 },
-              { label: "Nov", y: 28 },
-              { label: "Dec", y: 100 }
-            ];
-            $scope.chart.options.data[1].dataPoints = [
-              { label: "Jan", y: 16 },
-              { label: "Feb", y: 14 },
-              { label: "Mar", y: 25 },
-              { label: "Apr", y: 30 },
-              { label: "May", y: 28 },
-              { label: "Jun", y: 28 },
-              { label: "Jul", y: 57 },
-              { label: "Aug", y: 18 },
-              { label: "Sep", y: 60 },
-              { label: "Oct", y: 68 },
-              { label: "Nov", y: 28 },
-              { label: "Dec", y: 100 }
-            ];
-          } else if (updateBy == 'y') {
-            $scope.chart.options.title.text = "Yearly";
-            $scope.chart.options.data[0].dataPoints = [
-              { label: "2014", y: 10 },
-              { label: "2015", y: 15 },
-              { label: "2016", y: 25 },
-              { label: "2017", y: 30 },
-              { label: "2018", y: 28 }
-            ];
-            $scope.chart.options.data[1].dataPoints = [
-              { label: "2014", y: 10 },
-              { label: "2015", y: 15 },
-              { label: "2016", y: 25 },
-              { label: "2017", y: 30 },
-              { label: "2018", y: 28 }
-            ];
-          } else {
-            $scope.chart.options.title.text = "Weekly";
-            $scope.chart.options.data[0].dataPoints = [
-              { label: "1/7/2017", y: 10 },
-              { label: "8/7/2017", y: 15 },
-              { label: "15/7/2017", y: 25 },
-              { label: "21/7/2017", y: 30 },
-              { label: "28/8/2017", y: 28 }
-            ];
-            $scope.chart.options.data[1].dataPoints = [
-              { label: "1/7/2017", y: 11 },
-              { label: "8/7/2017", y: 14 },
-              { label: "15/7/2017", y: 29 },
-              { label: "21/7/2017", y: 40 },
-              { label: "28/8/2017", y: 58 }
-            ];
-          }
-          $scope.chart.render();
-        }
-
-        $scope.updateChart('w');
-
+        $scope.masterData = {
+          "chartType": ["Revenue", "Sales"],
+          "month": ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+          "year": ["2017"]
+        };
 
         $("#reviewDate").datepicker({
           changeMonth: true,
@@ -272,6 +178,119 @@ angular.
             }
           }
 
+        }
+
+        $scope.ctChange = function (data) {
+          $scope.chartType = data;
+        }
+
+        $scope.mnChange = function (data) {
+          $scope.selectedMonth = data;
+        }
+
+        $scope.yrChange = function (data) {
+          $scope.selectedYear = data;
+        }
+
+        $scope.updateRenderChartBy = function (data) {
+          $scope.renderChartBy = data;
+        }
+
+        $scope.generateChart = function () {
+          $scope.chart = new CanvasJS.Chart("ct-visits", {
+            title: {
+              text: "Adding & Updating dataPoints"
+            },
+            data: [
+              {
+                // Change type to "doughnut", "line", "splineArea", etc.
+                type: "column",
+                dataPoints: [
+                  { label: "1/7/2017", y: 10 },
+                  { label: "8/7/2017", y: 15 },
+                  { label: "15/7/2017", y: 25 },
+                  { label: "21/7/2017", y: 30 },
+                  { label: "28/8/2017", y: 28 }
+                ]
+              },
+              {
+                // Change type to "doughnut", "line", "splineArea", etc.
+                type: "line",
+                dataPoints: [
+                  { label: "1/7/2017", y: 11 },
+                  { label: "8/7/2017", y: 14 },
+                  { label: "15/7/2017", y: 29 },
+                  { label: "21/7/2017", y: 40 },
+                  { label: "28/8/2017", y: 58 }
+                ]
+              }
+            ]
+          });
+          var updateBy = $scope.renderChartBy;
+          if (updateBy == 'm') {
+            $scope.chart.options.title.text = "Monthly";
+            $scope.chart.options.data[0].dataPoints = [
+              { label: "Jan", y: 10 },
+              { label: "Feb", y: 15 },
+              { label: "Mar", y: 25 },
+              { label: "Apr", y: 30 },
+              { label: "May", y: 28 },
+              { label: "Jun", y: 28 },
+              { label: "Jul", y: 50 },
+              { label: "Aug", y: 28 },
+              { label: "Sep", y: 60 },
+              { label: "Oct", y: 28 },
+              { label: "Nov", y: 28 },
+              { label: "Dec", y: 100 }
+            ];
+            $scope.chart.options.data[1].dataPoints = [
+              { label: "Jan", y: 16 },
+              { label: "Feb", y: 14 },
+              { label: "Mar", y: 25 },
+              { label: "Apr", y: 30 },
+              { label: "May", y: 28 },
+              { label: "Jun", y: 28 },
+              { label: "Jul", y: 57 },
+              { label: "Aug", y: 18 },
+              { label: "Sep", y: 60 },
+              { label: "Oct", y: 68 },
+              { label: "Nov", y: 28 },
+              { label: "Dec", y: 100 }
+            ];
+          } else if (updateBy == 'y') {
+            $scope.chart.options.title.text = "Yearly";
+            $scope.chart.options.data[0].dataPoints = [
+              { label: "2014", y: 10 },
+              { label: "2015", y: 15 },
+              { label: "2016", y: 25 },
+              { label: "2017", y: 30 },
+              { label: "2018", y: 28 }
+            ];
+            $scope.chart.options.data[1].dataPoints = [
+              { label: "2014", y: 10 },
+              { label: "2015", y: 15 },
+              { label: "2016", y: 25 },
+              { label: "2017", y: 30 },
+              { label: "2018", y: 28 }
+            ];
+          } else {
+            $scope.chart.options.title.text = "Daily";
+            $scope.chart.options.data[0].dataPoints = [
+              { label: "1/7/2017", y: 10 },
+              { label: "8/7/2017", y: 15 },
+              { label: "15/7/2017", y: 25 },
+              { label: "21/7/2017", y: 30 },
+              { label: "28/8/2017", y: 28 }
+            ];
+            $scope.chart.options.data[1].dataPoints = [
+              { label: "1/7/2017", y: 11 },
+              { label: "8/7/2017", y: 14 },
+              { label: "15/7/2017", y: 29 },
+              { label: "21/7/2017", y: 40 },
+              { label: "28/8/2017", y: 58 }
+            ];
+          }
+          $scope.chart.render();
         }
       }
     ]
