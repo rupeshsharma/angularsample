835a87a8b90113cb614e181be6bc7cc27ee573fa
rupeshsharma <rupesh.sharma@globallogic.com>
2017-09-14 17:50:44 +0530

    Update staff.component.js

--- component/staff/staff.component.js
+++ component/staff/staff.component.js
@@ -2,37 +2,33 @@ angular.
   module('sample').
   component('staff', {
     templateUrl: './component/staff/staff.template.html',
-    controller: ['$scope', '$timeout', '$rootScope', '$location', 'sessionService', 'customerService',
-      function staffController($scope, $timeout, $rootScope, $location, sessionService, customerService) {
+    controller: ['$scope', '$timeout', '$rootScope', '$location', 'sessionService', 'userService',
+      function staffController($scope, $timeout, $rootScope, $location, sessionService, userService) {
 
-         $scope.$on('staffInit', function (event) {
-           $scope.staffInit();
+        $scope.isEdit = false;
+
+        $scope.$on('staffInit', function (event) {
+          $scope.staffInit();
         });
 
         $scope.staffInit = function () {
-           document.getElementById("loadingIndicator").style.display = 'block';
-           document.getElementById("staffComponent").style.display = 'none';
-           $timeout(function () {
-             document.getElementById("loadingIndicator").style.display = 'none';
-             document.getElementById("staffComponent").style.display = 'block';
-        }, 500);
-          console.debug("Staff INIT");
+          getStaffList();
         }
 
-        $scope.staffList = [
-          {
-            "id": 1,
-            "name": "Rupesh Sharma",
-            "mobile": "8826479790",
-            "type": "Admin"
-          }, {
-            "id": 2,
-            "name": "Devendra Sharma",
-            "mobile": "9691639313",
-            "type": "Admin"
-          }
+        function closeLoadingIndicator() {
+          document.getElementById("loadingIndicator").style.display = 'none';
+          document.getElementById("staffComponent").style.display = 'block';
+        }
 
-        ];
+        function getStaffList() {
+          document.getElementById("loadingIndicator").style.display = 'block';
+          document.getElementById("staffComponent").style.display = 'none';
+          userService.getStaffList(data => {
+            $scope.staffList = data;
+            closeLoadingIndicator();
+          });
+        }
+
       }
     ]
   });
