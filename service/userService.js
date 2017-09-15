f2ede7a1370ae4aaba46ff94e2640a0b565dea39
rupeshsharma <rupesh.sharma@globallogic.com>
2017-09-15 14:22:05 +0530

    Update userService.js

--- service/userService.js
+++ service/userService.js
@@ -9,6 +9,30 @@ APP.factory('userService', ['httpService',
                         callback(resp.data);
                     }
                 );
+            },
+
+            updateUser: function (request, callback) {
+                httpService.updateUser(request).then(
+                    resp => {
+                        callback(resp.data);
+                    }
+                );
+            },
+
+            createUser: function (request, callback) {
+                httpService.createUser(request).then(
+                    resp => {
+                        callback(resp.data);
+                    }
+                );
+            },
+
+            changePassword: function (request, callback) {
+                httpService.changePassword(request).then(
+                    resp => {
+                        callback(resp.data);
+                    }
+                );
             }
         }
     }
