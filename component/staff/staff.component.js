44e47576d929de0694c06e6864a9080eac9f9b67
rupeshsharma <rupesh.sharma@globallogic.com>
2017-09-15 14:22:47 +0530

    Update staff.component.js

--- component/staff/staff.component.js
+++ component/staff/staff.component.js
@@ -6,7 +6,7 @@ angular.
       function staffController($scope, $timeout, $rootScope, $location, sessionService, userService) {
 
         $scope.isEdit = false;
-
+        $scope.staffFormData = {};
         $scope.$on('staffInit', function (event) {
           $scope.staffInit();
         });
@@ -27,6 +27,54 @@ angular.
             $scope.staffList = data;
             closeLoadingIndicator();
           });
+        }
+
+        $scope.showAddEditStaffForm = function (isEdit, data) {
+          $scope.selectedStaff = data;
+          $scope.isEdit = isEdit;
+          if (!isEdit) {
+            $scope.staffFormData = {};
+          } else {
+            $scope.staffFormData.id = data.id;
+            $scope.staffFormData.name = data.name;
+            $scope.staffFormData.mobile = data.mobile;
+            $scope.staffFormData.role = data.role;
+          }
+
+        }
+
+        $scope.updateStaff = function () {
+          document.getElementById("loadingIndicator").style.display = 'block';
+          document.getElementById("staffComponent").style.display = 'none';
+          userService.updateUser($scope.staffFormData, data => {
+            $scope.selectedStaff.name = data.name;
+            $scope.selectedStaff.mobile = data.mobile
+            $scope.selectedStaff.role = data.role;
+            closeLoadingIndicator();
+          });
+        }
+
+        $scope.createStaff = function () {
+          document.getElementById("loadingIndicator").style.display = 'block';
+          document.getElementById("staffComponent").style.display = 'none';
+          userService.createUser($scope.staffFormData, data => {
+            $scope.staffList.push(data);
+            closeLoadingIndicator();
+          });
+        }
+
+        $scope.changePassword = function () {
+          document.getElementById("loadingIndicator").style.display = 'block';
+          document.getElementById("staffComponent").style.display = 'none';
+          userService.changePassword($scope.staffFormData, data => {
+            closeLoadingIndicator();
+          });
+        }
+
+        $scope.showPasswordStaffForm = function (data) {
+          $scope.staffFormData = {};
+          $scope.staffFormData.id = data.id;
+          $scope.staffFormData.name = data.name;
         }
 
       }
