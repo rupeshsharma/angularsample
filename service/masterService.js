d5383a6c2ce1efea9f42f531d476f3c4b5ab9215
rupeshsharma <rupesh.sharma@globallogic.com>
2017-09-18 13:57:37 +0530

    Update masterService.js

--- service/masterService.js
+++ service/masterService.js
@@ -1,16 +1,37 @@
 var APP = angular.module('sample')
 
 APP.factory('masterService', ['httpService',
-    function(httpService) {
+    function (httpService) {
 
         return {
-            getMasterData: function getMasterData (callback){
-            httpService.getMasterData().then(
-            resp => {
-                callback(resp.data); 
+            getMasterData: function getMasterData(callback) {
+                httpService.getMasterData().then(
+                    resp => {
+                        callback(resp.data);
+                    }
+                );
+            },
+            addMasterDataValue: function (key, value, callback) {
+                httpService.addMasterDataValue(key, value).then(
+                    resp => {
+                        callback(resp);
+                    }
+                );
+            },
+            removeMasterDataValue: function (key, value, callback) {
+                httpService.removeMasterDataValue(key, value).then(
+                    resp => {
+                        callback(resp);
+                    }
+                );
+            },
+            updateMasterDataValue: function (key, value, callback) {
+                httpService.updateMasterDataValue(key, value).then(
+                    resp => {
+                        callback(resp);
+                    }
+                );
             }
-        );
-       }
-      }        
+        }
     }
-  ]);
+]);
