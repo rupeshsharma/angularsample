cb5114d23d1980230f3804afce0fc58e1490d48d
rupeshsharma <rupesh.sharma@globallogic.com>
2017-09-20 13:31:10 +0530

    Update login.component.js

--- component/login/login.component.js
+++ component/login/login.component.js
@@ -2,17 +2,30 @@ angular.
   module('sample').
   component('logIn', {
     templateUrl: './component/login/login.template.html',
-    controller: ['$scope', '$timeout', '$rootScope', '$location', 'sessionService', 'customerService', 'masterService',
-      function loginController($scope, $timeout, $rootScope, $location, sessionService, customerService, masterService) {
+    controller: ['$scope', '$timeout', '$rootScope', '$location', 'sessionService', 'masterService', 'base64Service', 'loginService',
+      function loginController($scope, $timeout, $rootScope, $location, sessionService, masterService, base64Service, loginService) {
+
+        $scope.credential = {};
 
         $scope.doLogin = function () {
           $("#loginComponent").addClass("disabledDiv");
           document.getElementById("loadingIndicator").style.display = 'block';
 
-          masterService.getMasterData(function (data) {
-            sessionService.setMasterData(data.masterData);
-            $location.path('/checkin');
-          });
+          loginService.authenticate($scope.credential, data => {
+            sessionService.setXAuthHeader(base64Service.encode($scope.credential.username + ":" + $scope.credential.password));
+            sessionService.setLoggedInUserData(data);
+
+            masterService.getMasterData(function (data) {
+              sessionService.setMasterData(data.masterData);
+              $location.path('/checkin');
+            });
+
+          }, errData => {
+            $("#loginComponent").removeClass("disabledDiv");
+            document.getElementById("loadingIndicator").style.display = 'none';
+            $scope.isLoginError = true;
+          })
+
         }
       }
     ]
