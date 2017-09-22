a4ff0ed9b064ef2930412e19c994a37c1039771c
rupeshsharma <rupesh.sharma@globallogic.com>
2017-09-22 12:54:45 +0530

    Update staff.component.js

--- component/staff/staff.component.js
+++ component/staff/staff.component.js
@@ -37,10 +37,18 @@ angular.
           } else {
             $scope.staffFormData.id = data.id;
             $scope.staffFormData.name = data.name;
-            $scope.staffFormData.mobile = data.mobile;
+            $scope.staffFormData.email = data.email;
+            $scope.staffFormData.contact = data.contact;
             $scope.staffFormData.role = data.role;
           }
 
+        }
+
+        $scope.removeStaff = function (data) {
+          userService.removeStaff(data.id, function () {
+            var index = $scope.staffList.indexOf(data);
+            $scope.staffList.splice(index, 1);
+          });
         }
 
         $scope.createOrUpdateStaff = function () {
@@ -49,14 +57,17 @@ angular.
           if ($scope.isEdit) {
             userService.updateUser($scope.staffFormData, data => {
               $scope.selectedStaff.name = data.name;
-              $scope.selectedStaff.mobile = data.mobile
+              $scope.selectedStaff.email = data.email
+              $scope.selectedStaff.contact = data.contact;
               $scope.selectedStaff.role = data.role;
               closeLoadingIndicator();
+              $("#staffModal .close").click();
             });
           } else {
             userService.createUser($scope.staffFormData, data => {
               $scope.staffList.push(data);
               closeLoadingIndicator();
+              $("#staffModal .close").click();
             });
           }
         }
@@ -64,8 +75,9 @@ angular.
         $scope.changePassword = function () {
           document.getElementById("loadingIndicator").style.display = 'block';
           document.getElementById("staffComponent").style.display = 'none';
-          userService.changePassword($scope.staffFormData, data => {
+          userService.changePassword({ "id": $scope.staffFormData.id, "password": $scope.newStaffPassword }, data => {
             closeLoadingIndicator();
+            $("#staffPasswordModal .close").click();
           });
         }
 
