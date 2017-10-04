f8d1c394c44fd41ae2110b1f9a70354de8cca5de
rupeshsharma <rupesh.sharma@globallogic.com>
2017-10-04 12:04:39 +0530

    Update customerStatus.component.js

--- component/order/status/customer/customerStatus.component.js
+++ component/order/status/customer/customerStatus.component.js
@@ -2,18 +2,18 @@ angular.
   module('sample').
   component('customerStatus', {
     templateUrl: './component/order/status/customer/customerStatus.template.html',
-    controller: ['$scope', '$timeout', '$rootScope', '$location', '$localStorage', '$interval',
-      function customerStatusController($scope, $timeout, $rootScope, $location, $localStorage, $interval) {
+    controller: ['$scope', '$timeout', '$rootScope', '$location', '$localStorage', '$interval', 'orderHistoryService',
+      function customerStatusController($scope, $timeout, $rootScope, $location, $localStorage, $interval, orderHistoryService) {
 
-        $scope.orderList = [{"orderNumber":111,"orderStatus":"Preparing"},
-        {"orderNumber":222,"orderStatus":"Preparing"},
-        {"orderNumber":333,"orderStatus":"Serving"},
-        {"orderNumber":444,"orderStatus":"Preparing"},
-        {"orderNumber":555,"orderStatus":"Received"},
-        {"orderNumber":666,"orderStatus":"Received"},
-        {"orderNumber":777,"orderStatus":"Preparing"}];
+        $rootScope.viewType = 'customerStatus';
 
+        function getOrderStatus() {
+          orderHistoryService.getCustomerOrderStatus(data => {
+            $scope.orderList = data;
+          });
+        }
 
+        $interval(getOrderStatus, 15000);
 
       }
     ]
